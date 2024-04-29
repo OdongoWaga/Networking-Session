b667ddee122722099fef1e33f06df6abb700bc28
Patrick Waga <patrickwaga@icloud.com>
2024-04-29 11:05:58 -0400

    Initial Commit

---
+++ URLSession/URLSessionApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright iOS Mastery.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
